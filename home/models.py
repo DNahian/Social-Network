8a159b0f9bc11fcd2e2b697fa2cee98a6ef22b95
Nahian Ismail <nahiyan313@.gmailcom>
2020-07-15 17:05:54 +0600

    home page update

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    post = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    date = models.DateTimeField(auto_now_add=True)
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from home.forms import HomeForm
+from home.models import Post
 
 
 class HomeView(TemplateView):
@@ -8,4 +9,19 @@ class HomeView(TemplateView):
 
     def get(self, request):
         form = HomeForm()
-        return render(request, self.template_name, {'form': form})
+        posts = Post.objects.all()
+        args = {'form': form, 'posts': posts}
+        return render(request, self.template_name, args)
+
+    def post(self, request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            text = form.cleaned_data['post']
+            form = HomeForm()
+            return redirect('home:home')
+
+        args = {'form': form, 'text': text}
+        return render(request, self.template_name, args)
